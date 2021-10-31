757d409e401c6a423069b5ddae8b085a07cc5bbc
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2021-10-31 18:13:06 +0200

    Create ConstructionCrew.js

---
+++ Object and Compositions - Exercise/ConstructionCrew.js
@@ -0,0 +1,13 @@
+function construction(worker){
+if(worker.dizziness){
+    worker.levelOfHydrated += worker.weight*0.1*worker.experience;
+    worker.dizziness == false;
+}
+
+return worker;
+}
+
+console.log (construction({ weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }));
