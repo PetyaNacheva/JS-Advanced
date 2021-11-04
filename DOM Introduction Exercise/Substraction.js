25484f8420e6972e9ed5f9b54e05668efc182d7f
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2021-11-04 18:58:55 +0200

    Delete Substraction.js

--- DOM Introduction Exercise/Substraction.js
+++
@@ -1,8 +0,0 @@
-function subtract(){
-    let firstNum = document.getElementById('firstNumber').value;
-    let secNum = document.getElementById('secondNumber').value;
-    
-    let sum = Number(firstNum)-Number(secNum);
-    document.getElementById('result').textContent= sum;
-    
-}
