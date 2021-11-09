695253b95995cca6db9124828685a48ac5fce004
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2021-11-09 18:52:32 +0200

    Create fibonacci.js

---
+++ AdvancedFunctions-Exercise/Fibonacci/fibonacci.js
@@ -0,0 +1,13 @@
+function getFibonator(){
+    let num1 = 0;
+    let num2 = 1;
+
+    return function fib(){
+        let result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+        return num1;
+    }
+}
+
+
